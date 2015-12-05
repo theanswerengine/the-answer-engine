c93d307b49c4e6253e039b25f1841d4f931d400a
theanswerengine <theanswerengine@gmail.com>
2015-12-05 05:41:17 +0000

    CHMOD 770

---
+++ what/vbz/mc/ii/mc/WebSubmittedAnswerer5662781cc9d3e.php
@@ -0,0 +1,24 @@
+<?php 
+/* 
+what / vbz / mc / ii / mc / 
+what / is / cardinal number,neutral for number (two, three..) / general preposition / cardinal number,neutral for number (two, three..) / 
+*/
+
+class WebSubmittedAnswerer5662781cc9d3e implements Answerer {
+    public static function check_question(array $question) {
+        return false; // This algorithm doesn't do anything yet.
+    }
+
+    public static function answer_question(array $question) {
+        /*
+
+          Author: Web user IP 65.182.48.83
+          Author's original question: 'what is two plus two?'
+
+          Comments:
+
+should be easy to check for certain values of general preposition sandwiched between cardinal numbers and just do the computation. 
+
+        */
+    }
+}
